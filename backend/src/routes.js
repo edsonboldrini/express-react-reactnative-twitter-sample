04d89e9e890e56f325bb01cf11e166051621dd73
Edson Boldrini <edsonboldrini@gmail.com>
2018-11-12 15:10:14 -0200

    Frontend added to the project

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const routes = express.Router();
 
-const TweetController = require("./controllers/TweeetController");
+const TweetController = require("./controllers/TweetController");
 const LikeController = require("./controllers/LikeController");
 
 routes.get("/tweets", TweetController.index);
